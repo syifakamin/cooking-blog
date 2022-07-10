a16dae5376578481550a481b450612d9f4ce4ca9
Amin Syifak <aminsyifak15@gmail.com>
2022-07-11 00:39:05 +0700

    feat: Add Explore Random Function

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -109,6 +109,22 @@ exports.exploreLatest = async (req, res) => {
     }
 }
 
+/** 
+ * Get /explore-random
+ * Explore random as JSON
+ */
+
+exports.exploreRandom = async (req, res) => {
+    try {
+        let count = await Recipe.find().countDocuments();
+        let random = Math.floor(Math.random() * count);
+        let recipe = await Recipe.findOne().skip(random).exec();
+        res.render('explore-random', { title: 'Cooking Blog - Explore Random', recipe });
+    } catch (error) {
+        res.satus(500).send({ message: error.message || "Error Occured" });
+    }
+}
+
 
 // async function insertDymmyRecipeData() {
 //     try {
